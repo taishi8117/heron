cd450ce3e6dd75400c81045634255e8b55960a32
Taishi Nojima <tnojima@twitter.com>
2016-06-16 17:25:14 -0700

    Gperftools testing

--- scripts/travis/ci.sh
+++ scripts/travis/ci.sh
@@ -6,19 +6,21 @@ DIR=`dirname $0`
 
 source ${DIR}/common.sh
 
-T="make site"
-start_timer "$T"
-(cd website && make site)
-end_timer "$T"
+#T="make site"
+#start_timer "$T"
+#(cd website && make site)
+#end_timer "$T"
+#
+#T="${DIR}/build.sh"
+#start_timer "$T"
+#${DIR}/build.sh
+#end_timer "$T"
+#
+#T="${DIR}/test.sh"
+#start_timer "$T"
+#${DIR}/test.sh
+#end_timer "$T"
 
-T="${DIR}/build.sh"
-start_timer "$T"
-${DIR}/build.sh
-end_timer "$T"
+bazel --bazelrc=tools/travis-ci/baezl.rc build --config=ubuntu //3rdparty/gperftools:gperftools-srcs
 
-T="${DIR}/test.sh"
-start_timer "$T"
-${DIR}/test.sh
-end_timer "$T"
-
-print_timer_summary
+#print_timer_summary
