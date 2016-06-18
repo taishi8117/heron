44cadde487648a2a60960953a8f49f6bc833f02c
Taishi Nojima <tnojima@twitter.com>
2016-06-17 17:09:02 -0700

    try again -- travis just trying to build gperftools

--- scripts/travis/ci.sh
+++ scripts/travis/ci.sh
@@ -6,19 +6,22 @@ DIR=`dirname $0`
 
 source ${DIR}/common.sh
 
-T="make site"
-start_timer "$T"
-(cd website && make site)
-end_timer "$T"
+#T="make site"
+#start_timer "$T"
+#(cd website && make site)
+#end_timer "$T"
+#
+#T="${DIR}/build.sh"
+#start_timer "$T"
+#${DIR}/build.sh
+#end_timer "$T"
+#
+#T="${DIR}/test.sh"
+#start_timer "$T"
+#${DIR}/test.sh
+#end_timer "$T"
 
-T="${DIR}/build.sh"
-start_timer "$T"
-${DIR}/build.sh
-end_timer "$T"
+bazel --bazelrc=tools/travis-ci/bazel.rc build --config=ubuntu //3rdparty/glog:glog-srcs --verbose_failures
+bazel --bazelrc=tools/travis-ci/bazel.rc build --config=ubuntu //3rdparty/gperftools:gperftools-srcs --verbose_failures
 
-T="${DIR}/test.sh"
-start_timer "$T"
-${DIR}/test.sh
-end_timer "$T"
-
-print_timer_summary
+#print_timer_summary
