de0e1841a05ba10b6775deb56a9d16ea01be8f08
Taishi Nojima <tnojima@twitter.com>
2016-08-12 18:17:13 -0700

    still working on packaging"

--- scripts/packages/create_pyheron_library.sh
+++ scripts/packages/create_pyheron_library.sh
@@ -7,6 +7,8 @@ PYHERON_TOPLEVEL_DIR="${TMP_DIR}/pyheron"
 echo "PyHeron toplevel directory: ${PYHERON_TOPLEVEL_DIR}"
 HERON_DIR=`git rev-parse --show-toplevel`
 
+cd ${HERON_DIR}
+
 # directory structure
 # pyheron
 # |- heron/
@@ -29,9 +31,23 @@ HERON_DIR=`git rev-parse --show-toplevel`
 # creates a directory
 mkdir -p $PYHERON_TOPLEVEL_DIR
 
-# heron/common and corresponding __init__.py
-mkdir -p ${PYHERON_TOPLEVEL_DIR}/heron/common/src/python
+################################### heron/common ###################################################
+mkdir -p ${PYHERON_TOPLEVEL_DIR}/heron/common/src
 
+# recursively create __init__.py
+for DIR in heron heron/common heron/common/src
+do
+  touch ${PYHERON_TOPLEVEL_DIR}/${DIR}/__init__.py
+done
+
+# copy dir from heron/common/src/python
+cp -R ${HERON_DIR}/heron/common/src/python ${PYHERON_TOPLEVEL_DIR}/heron/common/src/python
+
+#################################### heron/proto ###################################################
+
+############################## heron/streamparse -> pyheron ########################################
 
 # copy directory heron/streamparse/src/python to pyheron/pyheron
 cp -R ${HERON_DIR}/heron/streamparse/src/python ${PYHERON_TOPLEVEL_DIR}/pyheron
+
+tree $PYHERON_TOPLEVEL_DIR
