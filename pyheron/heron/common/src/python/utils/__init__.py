3f70d4ebd3615733afb20deae4d00229dc05ecf0
Taishi Nojima <tnojima@twitter.com>
2016-08-12 18:40:55 -0700

    Still packaging

--- pyheron/heron/common/src/python/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-'''common utility modules'''
-__all__ = ['metrics', 'misc', 'topology', 'config', 'tracker_access',
-           'tuple', 'log']
--- scripts/packages/create_pyheron_library.sh
+++ scripts/packages/create_pyheron_library.sh
@@ -4,10 +4,10 @@ set -e
 
 TMP_DIR=`mktemp -d`
 PYHERON_TOPLEVEL_DIR="${TMP_DIR}/pyheron"
-echo "PyHeron toplevel directory: ${PYHERON_TOPLEVEL_DIR}"
+UNZIP_DEST="${TMP_DIR}/unzipped"
 HERON_DIR=`git rev-parse --show-toplevel`
+BUILT_PKG_PATH=${HERON_DIR}/bazel-bin/heron/streamparse/src/python/pyheron_pkg.pex
 
-cd ${HERON_DIR}
 
 # directory structure
 # pyheron
@@ -28,26 +28,19 @@ cd ${HERON_DIR}
 # \_ requirements.txt
 
 
-# creates a directory
+# build pyheron_pkg pex file
+cd ${HERON_DIR}
+bazel build heron/streamparse/src/python:pyheron_pkg
+unzip -d $UNZIP_DEST $BUILT_PKG_PATH
+
+# creates a pyheron directory
 mkdir -p $PYHERON_TOPLEVEL_DIR
+# copis everything under heron to pyheron dir
+cp -R ${UNZIP_DEST}/heron ${PYHERON_TOPLEVEL_DIR}
+# move streamparse/src/python -> pyheron
+mv ${PYHERON_TOPLEVEL_DIR}/heron/streamparse/src/python ${PYHERON_TOPLEVEL_DIR}/pyheron
+rm -rf ${PYHERON_TOPLEVEL_DIR}/heron/streamparse
 
-################################### heron/common ###################################################
-mkdir -p ${PYHERON_TOPLEVEL_DIR}/heron/common/src
 
-# recursively create __init__.py
-for DIR in heron heron/common heron/common/src
-do
-  touch ${PYHERON_TOPLEVEL_DIR}/${DIR}/__init__.py
-done
-
-# copy dir from heron/common/src/python
-cp -R ${HERON_DIR}/heron/common/src/python ${PYHERON_TOPLEVEL_DIR}/heron/common/src/python
-
-#################################### heron/proto ###################################################
-
-############################## heron/streamparse -> pyheron ########################################
-
-# copy directory heron/streamparse/src/python to pyheron/pyheron
-cp -R ${HERON_DIR}/heron/streamparse/src/python ${PYHERON_TOPLEVEL_DIR}/pyheron
-
+echo "PyHeron toplevel directory: ${PYHERON_TOPLEVEL_DIR}"
 tree $PYHERON_TOPLEVEL_DIR
